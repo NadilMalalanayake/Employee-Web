472c0cdcd34c1eb1de4e1d2b2fec7c1f008f0ed0
Nadil Malalanayake <nadilviksura@gmail.com>
2024-06-18 17:50:01 +0530

    Create Post User Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './pages/header/Header';
+import Dashboard from './pages/dashboard/Dashboard';
+import Nomatch from './pages/nomatch/nomatch';
+import Postuser from './pages/employee/Postuser';
+
+
+
 
 function App() {
+
   return (
     <>
       <Header />
+      <Routes>
+          <Route path="/"  element={<Dashboard/>} />
+          <Route path="/employee"  element={<Postuser/>} />
+          <Route path="*"  element={<Nomatch/>} />
+      </Routes>
+
     </>
   );
 }
